1665df1a999f753e513796e1ee156fcbddc1e33b
Sophie <wenxi1988@hotmail.com>
2022-01-02 20:39:49 -0500

    environment configuration:react-router,sass,css-module

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.App}>
+     Sophie's Website
+    </div>
+  );
+}
+
+export default App;
