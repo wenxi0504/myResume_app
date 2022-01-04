90699e692210b4bdce02fb750bdb6e39729bc7f5
Sophie <wenxi1988@hotmail.com>
2022-01-04 00:52:38 -0500

    add portfolio

---
+++ .history/src/components/WorkLists/index_20220104002941.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import styles from './work.module.scss';
+import { Link } from 'react-router-dom';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ dataSource }) =>{
+    return <div className={ styles.worklist }>
+        <ul>
+            {
+                dataSource && dataSource.map(item=>
+                    <li key={item.id}>
+                       <Link to={item.Link}>
+                            <div className={ styles.cover }>
+                                <img src={item.cover}  alt="portfolio"/>
+                            </div>
+                            <div className={styles.workbrief}>
+                                <h3>{item.title}</h3>
+                                <p> {item.brief}</p>
+                            </div> 
+                            <div className={styles.tag}>
+                                    { item.tags && item.tags.map(i=><span key={i}>{i}</span>) }
+                                    </div>   
+                        </Link>
+                    </li>
+                )
+            }
+        </ul>
+    </div>
+}
---
+++ .history/src/components/WorkLists/index_20220104004853.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import styles from './work.module.scss';
+import { Link } from 'react-router-dom';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ dataSource }) =>{
+    return <div className={ styles.worklist }>
+        <ul>
+            {
+                dataSource && dataSource.map(item=>
+                    <li key={item.id}>
+                            <div className={ styles.cover }>
+                                <img src={item.cover}  alt="portfolio"/>
+                            </div>
+                            <div className={styles.workbrief}>
+                                <h3> <a href={item.link}>{item.title}</a></h3>
+                                <p> {item.brief}</p>
+                            </div> 
+                            <div className={styles.tag}>
+                                    { item.tags && item.tags.map(i=><span key={i}>{i}</span>) }
+                                    </div>  
+                    </li>
+                )
+            }
+        </ul>
+    </div>
+}
